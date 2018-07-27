ba2928b38f89d48caebde905c08df0e4c2470006
MiracleHao <james19961119@gmail.com>
2018-07-27 20:41:55 +0800

    7/27

--- app/src/main/java/com/jsaw/ibreast/note/note_treat_main.java
+++ app/src/main/java/com/jsaw/ibreast/note/note_treat_main.java
@@ -1,58 +1,83 @@
 package com.jsaw.ibreast.note;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.jsaw.ibreast.R;
 
 public class note_treat_main extends AppCompatActivity {
-    private FragmentManager manager;
+
+    //透過下方程式碼，取得Activity中執行的個體。
     private FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_main);
+        transaction = getSupportFragmentManager().beginTransaction();
 
-        //透過下方程式碼，取得Activity中執行的個體。
-        manager = getSupportFragmentManager();
+//         first fragment is note_treat1
+        transaction.replace(R.id.center, new note_treat1(), "fragment1");
+        transaction.commit();
     }
 
     //換頁的按鈕設定
     public void onClick(View view) {
-        //透過下方程式碼，取得Activity中執行的個體。
-        transaction = manager.beginTransaction();
+        transaction = getSupportFragmentManager().beginTransaction();
+        //傳回被按的button
+        int btnC;
         switch (view.getId()) {
             case R.id.btn1:
-                note_treat1 fragment1 = new note_treat1();
-                transaction.replace(R.id.center, fragment1, "fragment1");
-
+                btnC = 1;
+                setColor(btnC);
+                transaction.replace(R.id.center, new note_treat1(), "fragment1");
                 break;
             case R.id.btn2:
-                note_treat2 fragment2 = new note_treat2();
-                transaction.replace(R.id.center, fragment2, "fragment2");
+                btnC = 2;
+                setColor(btnC);
+                transaction.replace(R.id.center, new note_treat2(), "fragment2");
                 break;
             case R.id.btn3:
-                note_treat3 fragment3 = new note_treat3();
-                transaction.replace(R.id.center, fragment3, "fragment3");
+                btnC = 3;
+                setColor(btnC);
+                transaction.replace(R.id.center, new note_treat3(), "fragment3");
                 break;
             case R.id.btn4:
-                note_treat3 fragment4 = new note_treat3();
-                transaction.replace(R.id.center, fragment4, "fragment4");
+                btnC = 4;
+                setColor(btnC);
+                transaction.replace(R.id.center, new note_treat3(), "fragment4");
                 break;
             case R.id.btn5:
-                note_treat3 fragment5 = new note_treat3();
-                transaction.replace(R.id.center, fragment5, "fragment5");
+                btnC = 5;
+                setColor(btnC);
+                transaction.replace(R.id.center, new note_treat3(), "fragment5");
                 break;
             case R.id.Btn_edit:
-                note_treat2_add fragment2a = new note_treat2_add();
-                transaction.replace(R.id.center, fragment2a, "fragment2a");
+                transaction.replace(R.id.center, new note_treat2_add(), "fragment2a");
                 break;
         }
-//呼叫commit讓變更生效。
+        //呼叫commit讓變更生效。
         transaction.commit();
+    }
+
+    public void setColor(int btnC) {
+        int countFragment = 5;
+        Button btn[] = new Button[]{findViewById(R.id.btn1), findViewById(R.id.btn2),
+                findViewById(R.id.btn3), findViewById(R.id.btn4), findViewById(R.id.btn5)};
+
+        for (int i = 0; i < countFragment; i++) {
+            if (i == btnC - 1) {
+                btn[i].setTextColor(Color.WHITE);
+            } else {
+                btn[i].setTextColor(Color.GRAY);
+            }
+        }
     }
 }
