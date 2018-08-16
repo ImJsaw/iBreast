b2a629f485e9d604d55bfbddd2515155da8a2ecf
= <bing5300@gamil.com>
2018-08-16 22:58:10 +0800

    ask template complete

--- app/src/main/java/com/jsaw/ibreast/ask.java
+++ app/src/main/java/com/jsaw/ibreast/ask.java
@@ -24,69 +24,38 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
-public class cure extends AppCompatActivity {
-    private String url[] = new String[50];
-    private List<listAdapter.listData> mData = null;
-    private listAdapter mAdapter = null;
+public class ask extends AppCompatActivity {
+    private LinkedList<askAdapter.askData> mData = null;
+    private askAdapter mAdapter = null;
     private ListView listView;
-    private ProgressDialog progressDialog;
-    private Boolean isProgressDialogShow = false;
+    final int askCount = 3;
+    private String[] title;
+    private String[] ans ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cure);
+        title = getResources().getStringArray(R.array.ask_title);
+        ans = getResources().getStringArray(R.array.ask_ans);
+        setContentView(R.layout.activity_ask);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);//display "back" on action bar
-        listView = findViewById(R.id.list_cure);
-        //waiting dialog
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("處理中,請稍候...");
-        progressDialog.show();
-        isProgressDialogShow = true;
-        //connect time out
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (isProgressDialogShow) {
-                    progressDialog.dismiss();
-                    Toast.makeText(cure.this, "連線逾時", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }, 5000);
+        listView = findViewById(R.id.list_ask);
+
         getData();
     }
 
     private void getData() {
         mData = new LinkedList<>();
-        FirebaseDatabase.getInstance().getReference("cure").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (int i = 0; i < dataSnapshot.getChildrenCount(); i++) {
-                    listAdapter.listData c = new listAdapter.listData();
-                    c.title = Objects.requireNonNull(dataSnapshot.child(String.valueOf(i)).child("title").getValue()).toString();
-                    c.url = Objects.requireNonNull(dataSnapshot.child(String.valueOf(i)).child("url").getValue()).toString();
-                    url[i] = c.url;
-                    mData.add(c);
-                }
-                mAdapter.notifyDataSetChanged();
-                progressDialog.dismiss();
-                isProgressDialogShow = false;
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-            }
-        });
+        for (int i = 0;i < askCount;i++){
+            askAdapter.askData a = new askAdapter.askData();
+            a.title = title[i];
+            a.text = ans[i];
+            mData.add(a);
+        }
         //listView
-        mAdapter = new listAdapter((LinkedList<listAdapter.listData>) mData, cure.this);
+        mAdapter = new askAdapter( mData, ask.this);
         listView.setAdapter(mAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.d("URL:", url[position]);
-                startActivity(new Intent().setClass(cure.this, open_pdf.class).putExtra("URL", url[position]));
-            }
-        });
     }
 
     //action bar
--- app/src/main/java/com/jsaw/ibreast/askAdapter.java
+++ app/src/main/java/com/jsaw/ibreast/askAdapter.java
@@ -5,21 +5,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.HashMap;
 import java.util.LinkedList;
 
-public class listAdapter extends BaseAdapter {
-    private LinkedList<listData> mData;
+public class askAdapter extends BaseAdapter {
+    private LinkedList<askData> mData;
     private Context mContext;
+    private int currentItem = -1; //clicked item log
 
-    listAdapter(LinkedList<listData> mData, Context mContext) {
+    askAdapter(LinkedList<askData> mData, Context mContext) {
         this.mData = mData;
         this.mContext = mContext;
     }
 
-    static class listData {
+    private static class ViewHolder {
+        private LinearLayout showArea;
+        private TextView ask_list_title;
+        private TextView ask_list_text;
+        private RelativeLayout hideArea;
+    }
+
+    static class askData {
         String title;
-        String url;
+        String text;
     }
 
     @Override
@@ -29,7 +41,7 @@ public class listAdapter extends BaseAdapter {
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return mData.get(position);
     }
 
     @Override
@@ -38,10 +50,46 @@ public class listAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View v, ViewGroup parent) {
-        v = LayoutInflater.from( mContext ).inflate( R.layout.list_view, parent, false );
-        TextView t = v.findViewById(R.id.text);
-        t.setText(mData.get(position).title);
-        return v;
+    public View getView(final int position, View view, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (view == null) {
+            view = LayoutInflater.from(mContext).inflate(R.layout.ask_view, parent, false);
+            holder = new ViewHolder();
+            holder.showArea =  view.findViewById(R.id.layout_showArea);
+            holder.ask_list_title =  view.findViewById(R.id.ask_list_title);
+            holder.ask_list_text =  view.findViewById(R.id.ask_list_text);
+            holder.hideArea = view.findViewById(R.id.layout_hideArea);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+        askData item = mData.get(position);
+        // set tag
+        holder.showArea.setTag(position);
+        holder.ask_list_title.setText(item.title);
+        holder.ask_list_text.setText(item.text);
+        //set visible of clicked
+        if (currentItem == position) {
+            holder.hideArea.setVisibility(View.VISIBLE);
+        } else {
+            holder.hideArea.setVisibility(View.GONE);
+        }
+        holder.showArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int tag = (Integer) view.getTag();
+                if (tag == currentItem) { //click again
+                    currentItem = -1; //give a usable position ==> all hide
+                } else {
+                    currentItem = tag;
+                }
+                //renew
+                notifyDataSetChanged();
+            }
+        });
+        return  view;
     }
+
+
 }
+
